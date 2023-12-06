60cd40ee6bb8e11eaff00beb1575d1ac69ff77c3
Joel Chambers <104284414+Boomboomyoyo@users.noreply.github.com>
2023-12-05 21:36:50 -0700

    Partial Solution

--- README.md
+++ README.md
@@ -51,3 +51,12 @@ Test your new function; I've provided some basic testing code in `code.test.js`.
 What is the worst-case asymptotic time complexity of your implementation? What
 is the worst-case asymptotic memory complexity? Add your answer, including your
 reasoning, to this markdown file.
+
+
+# Response
+
+## Runtime Analysis
+
+## References
+Grabbed a quick array shuffler algorithm from here for the starting array
+https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
--- code.js
+++ code.js
@@ -1,3 +1,42 @@
 function tsp_ls(distance_matrix) {
-    return -1;
+    path = []
+    for(i = 0; i < distance_matrix.length; i++)
+    {
+        path.push(i)
+    }
+    shuffleArray(path)
+
+
+
 }
+
+function twoOptSwap(route, i, k)
+{
+    temp = route.splice(i, k)
+    route.splice(i, 0, temp.reverse())
+    console.log(route)
+    route = route.flat(Infinity)
+    console.log(route)
+}
+
+/*
+2optSwap(route, i, k)
+  cities 1 to i-1 stay in the order they are
+  cities i to k are reversed
+  cities k + 1 to n stay in the order they are*/
+
+
+// Array shuffler, for random starting array
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+arr = [0,1,2,3,4,5,6]
+twoOptSwap(arr, 2, 4)
+//arr = arr.flat()
+console.log(arr)
